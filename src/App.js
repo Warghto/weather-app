12f492ef189803cd8c45bf8ae858d3115c54508f
Avoid <116073567+Warghto@users.noreply.github.com>
2023-05-15 13:45:15 +0000

    Markup for web

--- src/App.js
+++ src/App.js
@@ -6,14 +6,29 @@ import axios from "axios";
 export default function App() {
   // const URL = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=d45bf05f66307ef1483c9b1231dd4b44'
   return (
-    <div className="App">
+    <div className="app">
       <div className="container">
         <div className="top">
           <div className="location">London</div>
-          <div className="temp">60 `C</div>
+          <div className="temp">
+            <h1>60 `C</h1>
+          </div>
           <div className="description">Windy</div>
         </div>
-        <div className="bottom"></div>
+        <div className="bottom">
+          <div className="feels">
+            <p className="bold">64 `C</p>
+            <p>Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className="wind">
+            <p>12 MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
       </div>
     </div>
   );
